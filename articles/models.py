ff9640de8544b56537b27b7c8c8599f5e1ccdd17
suhy <hisuhy065@gmail.com>
2024-08-06 14:32:40 +0900

    article Create 기능 구현

--- articles/models.py
+++ articles/models.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.contrib.auth import get_user_model
 
 # Create your models here.
-class Ariticle(models.Model):
+class Article(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     # 유저 모델을 참조하는 방법
