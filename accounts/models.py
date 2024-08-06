64c454fd0902c1f6993688b4f756a056eb84bb71
suhy <hisuhy065@gmail.com>
2024-08-06 14:01:48 +0900

    article 모델링/마이그레이션

--- accounts/models.py
+++ accounts/models.py
@@ -4,3 +4,4 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
     pass
+    # article_set = 자동으로 생성
--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from accounts.models import User
+from django.conf import settings
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+class Ariticle(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    # 유저 모델을 참조하는 방법
+    
+    # 1. 직접참조(권장하지 않음)
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    # 2. settings의 변수 활용
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    # 3. get_user_model
+    # user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+    # user_id = 자동으로 생성
